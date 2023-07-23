d581df4c7a02ee051f22fd29985239a904c7b5f7
pengcon <vpdzjs@gmail.com>
2023-07-24 05:09:26 +0900

    2023-07-24

---
+++ app/src/main/java/com/example/graduationproject/DTO.java
@@ -0,0 +1,29 @@
+package com.example.graduationproject;
+
+public class DTO {
+
+    private double mapx;
+    private double mapy;
+    private String title;
+    public double getmapx() {
+        return mapx;
+    }
+    public void setmapx(double mapx) {
+        this.mapx = mapx;
+    }
+    public double getmapy() {
+        return mapy;
+    }
+    public void setmapy(double mapy) {
+        this.mapy = mapy;
+    }
+    public String gettitle() {
+        return title;
+    }
+    public void settitle(String name) {
+        this.title = name;
+    }
+
+
+
+}
